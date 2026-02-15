59a84b4dea0e7b99eeec70ad290fa38636e0f8c6
huydeptraicuto02-design <huydeptraicuto02@gmail.com>
2026-02-15 14:44:44 +0700

    app.py

--- app.py
+++ app.py
@@ -234,3 +234,9 @@ if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
   
+import os
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+    
